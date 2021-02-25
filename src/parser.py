622f08f4ea531e958c08256f8d421d8b8188c5d4
EL Beinane Abed <abed-el-baghi.el-beinane@veolia.com>
2021-02-25 18:29:37 +0000

    first

--- src/parser.py
+++ src/parser.py
@@ -1,9 +1,27 @@
+import pandas as pd
 def parse(filename):
     with open(filename, 'r') as fi:
-        nbPizzas, nbTeam1, nbTeam2, nbTeam3 = map(int,fi.readline().split())
-        lines = fi.readlines().split()
-
+        global_time, nb_inter, nb_streets, nb_cars, car_score = map(int, fi.readline().split())
+        
+        lines = fi.readlines()
+        streets = []
+        paths = []
+        for line in lines[0: nb_streets]:
+            line = line.split(' ')
+            streets.append({
+                'start_at': int(line[0]),
+                'end_at': int(line[1]),
+                'name': line[2],
+                'L': int(line[3].replace('\n', ''))})
+        for line in lines[nb_streets: ]:
+            paths.append({
+                'P': int(line[0]),
+                'paths': line[1: ].replace('\n', '').split(' ')[1:]
+            })
+        
         dataset = {
-            'nbPizzas': nbPizzas
+            'streets': streets,
+            'paths': paths
         }
+
         return dataset
--- src/scorer.py
+++ src/scorer.py
@@ -2,4 +2,4 @@ def score(solution, dataset):
     res = 0
     for idx in solution:
         res += dataset['pizzas'][idx]
-    return 0 if res > dataset['knapsize'] else res
+    return 0 if res > dataset['knapsize'] else resp
